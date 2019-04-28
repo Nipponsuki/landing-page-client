85e430bb919a4b048083f1266837ef143fd8e6ff
Alex <alex.alexeev23061991@gmail.com>
2019-04-28 21:20:08 +0300

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import logo from '../images/logo-1.png'
 import styled from 'styled-components';
+import { styles } from '../utils'
 
 const StyledNavbar = styled.nav`
 
 img{
+	border-radius: 3px;
+	border: 2px solid ${styles.colors.darkAccent};
+	box-shadow: 0 1px 1px 0 rgba(0,0,0,0.14), 0 2px 1px -1px rgba(0,0,0,0.12), 0 1px 3px 0 rgba(0,0,0,0.20);
+
 	@media(max-width: 700px){
 		width: 80%;
 	}
 	@media(max-width: 500px){
 		width: 70%;
 	}
+
 }
 	
 `;
